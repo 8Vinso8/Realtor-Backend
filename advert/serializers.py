3824b26ba0a18d14aa616d69f7c8128cb1c60654
Vinso <8vinso8@gmail.com>
2023-01-15 22:36:59 +1000

    Add all images to advert serializer

--- advert/serializers.py
+++ advert/serializers.py
@@ -1,9 +1,17 @@
-from advert.models import Advert
+from advert.models import Advert, AdvertImage
 from rest_framework import serializers
 
 
+class ImagesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = AdvertImage
+        fields = ['image']
+
+
 class AdvertSerializer(serializers.HyperlinkedModelSerializer):
+    images = ImagesSerializer(many=True, read_only=True)
+
     class Meta:
         model = Advert
-        fields = ['id','url', 'owner', 'price', 'preview',
-                  'address', 'description', 'date']
+        fields = ['id', 'url', 'owner', 'price', 'preview',
+                  'address', 'description', 'date', 'images']
