c4ccc843647b4b29fb7d6269bee000d27a45e1c6
Vinso <8vinso8@gmail.com>
2023-02-03 15:06:36 +1000

    Add extended favorites serializer to user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from advert.serializers import AdvertSerializer
 
 from users.models import CustomUser
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    favorite_adverts = AdvertSerializer(many=True, read_only=True)
     class Meta:
         model = CustomUser
         fields = ['id', 'url', 'username', 'email', 'phone', 'favorite_adverts', 'is_realtor', 'clients']
