778b941f38ee31a0371606a2accc927aa2e20206
Vinso <8vinso8@gmail.com>
2023-02-03 11:28:58 +1000

    Made favorite field nullable in the User model

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ from advert.models import Advert
 
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=30, verbose_name="Телефон", blank=True)
-    favorite_adverts = models.ManyToManyField(to=Advert)
+    favorite_adverts = models.ManyToManyField(to=Advert, blank=True, related_name="favorite_adverts", verbose_name="Избранные объявления")
     is_realtor = models.BooleanField(default=False)
     clients = models.ManyToManyField("self", blank=True)
     def __str__(self):
