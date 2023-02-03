3c7173dd7f0f51722621dc5ecd3989e0371219e9
Vinso <8vinso8@gmail.com>
2023-02-04 02:25:16 +1000

    Rename param

--- advert/filters.py
+++ advert/filters.py
@@ -6,10 +6,10 @@ class AdvertFilter(filters.FilterSet):
   address = filters.CharFilter(field_name='address', lookup_expr='icontains')
   city = filters.CharFilter(field_name='city', lookup_expr='icontains')
   street = filters.CharFilter(field_name='street', lookup_expr='icontains')
-  obj_type = filters.CharFilter(field_name='obj_type', lookup_expr='exact')
+  advert_type = filters.CharFilter(field_name='advert_type', lookup_expr='exact')
   floor = filters.NumberFilter(field_name='floor', lookup_expr='exact')
   is_sold = filters.BooleanFilter(field_name='sold', lookup_expr='exact')
 
   class Meta:
     model = Advert
-    fields = ['price', 'address', 'city', 'street', 'obj_type', 'floor', 'is_sold']
+    fields = ['price', 'address', 'city', 'street', 'advert_type', 'floor', 'is_sold']
